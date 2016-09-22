1d7a45a7d6c993453b85097f4dbe4db802068089
LaurensOomen <oomen.laurens@gmail.com>
2016-09-22 15:13:51 +0200

    Created suggestions api, model and controller + bug fix on categories controller

---
+++ src/Models/Suggestion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Endurance.Models
+{
+	public class Suggestion
+	{
+		public int ID { get; set; }
+
+		public string subject { get; set; }
+
+		public string message { get; set; }
+	}
+}
