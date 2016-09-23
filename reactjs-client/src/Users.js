6f8ccf462b4e3a23355bf1193be190a5360d4deb
LaurensOomen <oomen.laurens@gmail.com>
2016-09-23 15:04:57 +0200

    user controller edits and frontend adaptions

--- reactjs-client/src/Users.js
+++ reactjs-client/src/Users.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
+
+import { getUsers } from './api/Users'
+
 class Users extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      users: []
     }
   }
   componentWillMount() {
+    getUsers().then( (result) => {
+      console.log(result);
+      this.setState({users: result});
+    });
   }
   render() {
     return (
-      <h3>test</h3>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Lastname</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.users.map(transaction =>
+            <tr key={ transaction.category}>
+              <td>{ transaction.category }</td>
+              <td>{ transaction.expense }</td>
+            </tr>
+          ) }
+        </tbody>
+      </table>
     );
   }
 }
