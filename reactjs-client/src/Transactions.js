f9255e37817f206256fca8ee2f5ecba0bdae48b7
LaurensOomen <oomen.laurens@gmail.com>
2016-09-23 09:57:51 +0200

    halfway creating the collapsible list for expenses

--- reactjs-client/src/Transactions.js
+++ reactjs-client/src/Transactions.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 
 import { VictoryPie } from 'victory';
 import { getTransactions } from './api/Transactions'
+import { Accordion, AccordionItem} from 'react-sanfona'
+import { getExpenses } from './api/Expenses'
+
 
 class Transactions extends Component {
   constructor(props) {
     super(props);
-    
+
+    this.expenses = {
+      
+    }
     this.state = {
       transactions: []
     }
@@ -17,6 +23,18 @@ class Transactions extends Component {
     });
   }
   render() {
+    <Accordion>
+                {[1, 2, 3, 4, 5].map((item) => {
+                    return (
+                        <AccordionItem title={'Item ${ item }'} slug={item} key={item}>
+                            <div>
+                                {`Item ${ item } content`}
+                                {item === 3 ? <p><img src="https://cloud.githubusercontent.com/assets/38787/8015584/2883817e-0bda-11e5-9662-b7daf40e8c27.gif" /></p> : null}
+                            </div>
+                        </AccordionItem>
+                    );
+                })}
+            </Accordion>
       return (
           <div>
             <table className='table'>
@@ -29,7 +47,19 @@ class Transactions extends Component {
               <tbody>
                 {this.state.transactions.map(transaction =>
                   <tr key={ transaction.category}>
-                    <td>{ transaction.category }</td>
+                    <td>
+                      <Accordion>
+                        {[1].map( item => {
+                          return (
+                            <AccordionItem title={`${ transaction.category } Expense` } slug={item} key={item}>
+                              <div>
+                                {`Expense ${ transaction.category } content`}
+                              </div>
+                            </AccordionItem>
+                          )
+                        })}
+                      </Accordion>
+                    </td>
                     <td>{ transaction.expense }</td>
                   </tr>
                 ) }
